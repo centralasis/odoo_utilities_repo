933ae1f596c3bc1a4a9c969139e1ba2202a2be2a
faraibernard <farai.bernard@centralasis.com>
2026-06-11 10:25:40 -0400

    first commit

---
+++ pos program/base_driver.py
@@ -0,0 +1,103 @@
+# Copyright (C) 2014-Today Akretion (http://www.akretion.com).
+# Copyright (C) 2015-Today: GRAP (http://www.grap.coop)
+# Copyright (C) 2004-Today: Odoo S.A (<http://odoo.com>).
+# @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import time
+import traceback
+from queue import Queue
+from threading import Lock, Thread
+
+from flask import jsonify
+
+from pywebdriver import app
+
+
+def check(installed, plugin):
+    def wrap(func):
+        def wrapped_func(*args, **kwargs):
+            if installed:
+                return func(*args, **kwargs)
+            else:
+                app.logger.warning(
+                    "The plugin %s can not be loaded as one of its dependency "
+                    "is missing. You can install them using pip or debian\n"
+                    "The pip package are : %s \n"
+                    "The debian package are : %s \n"
+                    "More information here : "
+                    "https://github.com/akretion/pywebdriver\n"
+                    "Return False to the API Call"
+                    % (plugin["name"], plugin["require_pip"], plugin["require_debian"])
+                )
+                return jsonify(jsonrpc="2.0", result=False)
+
+        return wrapped_func
+
+    return wrap
+
+
+class AbstractDriver(object):
+    """Abstract Driver Class"""
+
+    def __init__(self, *args, **kwargs):
+        self.status = {"status": "disconnected", "messages": []}
+
+
+class ThreadDriver(Thread, AbstractDriver):
+    def __init__(self, *args, **kwargs):
+        Thread.__init__(self)
+        AbstractDriver.__init__(self, *args, **kwargs)
+        self.queue = Queue()
+        self.lock = Lock()
+        self.vendor_product = None
+
+    def get_vendor_product(self):
+        return self.vendor_product
+
+    def lockedstart(self):
+        with self.lock:
+            if not self.is_alive():
+                self.daemon = True
+                self.start()
+
+    def set_status(self, status, message=None):
+        if status == self.status["status"]:
+            if message is not None and (
+                len(self.status["messages"]) == 0
+                or message != self.status["messages"][-1]
+            ):
+                self.status["messages"].append(message)
+        else:
+            self.status["status"] = status
+            if message:
+                self.status["messages"] = [message]
+            else:
+                self.status["messages"] = []
+
+    def process_task(self, task, timestamp, data):
+        return getattr(self, task)(data)
+
+    def push_task(self, task, data=None):
+        if not hasattr(self, task):
+            raise AttributeError("The method %s do not exist for the Driver" % task)
+        self.lockedstart()
+        self.queue.put((time.time(), task, data))
+
+    def run(self):
+        while True:
+            try:
+                timestamp, task, data = self.queue.get(True)
+                self.process_task(task, timestamp, data)
+            except Exception as e:
+                errmsg = (
+                    str(e)
+                    + "\n"
+                    + "-" * 60
+                    + "\n"
+                    + traceback.format_exc()
+                    + "-" * 60
+                    + "\n"
+                )
+                app.logger.error(errmsg)
---
+++ pos program/escpos_driver.py
@@ -0,0 +1,381 @@
+# Copyright (C) 2014-Today Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# @author Sylvain CALADOR <sylvain.calador@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+import base64
+import fnmatch
+import io
+import logging
+from configparser import NoOptionError
+from tkinter import Image
+
+import usb.core
+from flask import jsonify, render_template, request
+from netifaces import AF_INET, ifaddresses, interfaces
+from xmlescpos import Layout
+
+from pywebdriver import app, config, drivers
+
+from .base_driver import ThreadDriver
+from escpos.printer import Network as POSLanDriver
+
+meta = {
+    "name": "ESCPOS Printer",
+    "description": """This plugin add the support of ESCPOS Printer for your
+        pywebdriver""",
+    "require_pip": ["pyxmlescpos"],
+    "require_debian": [],
+}
+
+if (
+    config.has_option("escpos_driver", "device_type")
+    and config.get("escpos_driver", "device_type") == "serial"
+):
+    device_type = "serial"
+elif (
+    config.has_option("escpos_driver", "device_type")
+    and config.get("escpos_driver", "device_type") == "win32"
+):
+    device_type = "win32"
+else:
+    device_type = "usb"
+
+SUPPORTED_DEVICES = [
+    # Don't add 2 entries with the same vendor and product IDs
+    # Epson TM-T70, TM-T70II and Epson TM-P20 have the same vendor/product IDs
+    {"vendor": 0x04B8, "product": 0x0E03, "name": "Epson TM-T20"},
+    {"vendor": 0x04B8, "product": 0x0202, "name": "Epson TM-T70"},
+    {"vendor": 0x04B8, "product": 0x0E15, "name": "Epson TM-T20II"},
+    {"vendor": 0x04B8, "product": 0x0E28, "name": "Epson TM-T20III"},
+    {"vendor": 0x0525, "product": 0xA700, "name": "Netchip POS Printer Aures ODP 333"},
+    {"vendor": 0x1FC9, "product": 0x2016, "name": "EPOS"},
+
+]
+
+_logger = logging.getLogger(__name__)
+
+try:  # noqa C901
+    if device_type == "serial":
+        from escpos.printer import Serial as POSDriver
+    elif device_type == "win32":
+        import win32print
+        from escpos.printer import Win32Raw as POSDriver
+
+        PRINTER_STATUS_DICT = {
+            0: {"title": "AVAILABLE", "usable": True},
+            win32print.PRINTER_STATUS_PAUSED: {"title": "PAUSED", "usable": False},
+            win32print.PRINTER_STATUS_ERROR: {"title": "ERROR", "usable": False},
+            win32print.PRINTER_STATUS_PENDING_DELETION: {
+                "title": "PENDING_DELETION",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_PAPER_JAM: {
+                "title": "PAPER_JAM",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_PAPER_OUT: {
+                "title": "PAPER_OUT",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_MANUAL_FEED: {
+                "title": "MANUAL_FEED",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_PAPER_PROBLEM: {
+                "title": "PAPER_PROBLEM",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_OFFLINE: {"title": "OFFLINE", "usable": False},
+            win32print.PRINTER_STATUS_IO_ACTIVE: {
+                "title": "IO_ACTIVE",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_BUSY: {"title": "BUSY", "usable": False},
+            win32print.PRINTER_STATUS_PRINTING: {"title": "PRINTING", "usable": True},
+            win32print.PRINTER_STATUS_OUTPUT_BIN_FULL: {
+                "title": "OUTPUT_BIN_FULL",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_NOT_AVAILABLE: {
+                "title": "NOT_AVAILABLE",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_WAITING: {"title": "WAITING", "usable": True},
+            win32print.PRINTER_STATUS_PROCESSING: {
+                "title": "PROCESSING",
+                "usable": True,
+            },
+            win32print.PRINTER_STATUS_INITIALIZING: {
+                "title": "INITIALIZING",
+                "usable": True,
+            },
+            win32print.PRINTER_STATUS_WARMING_UP: {
+                "title": "WARMING_UP",
+                "usable": True,
+            },
+            win32print.PRINTER_STATUS_TONER_LOW: {"title": "TONER_LOW", "usable": True},
+            win32print.PRINTER_STATUS_NO_TONER: {"title": "NO_TONER", "usable": False},
+            win32print.PRINTER_STATUS_PAGE_PUNT: {
+                "title": "PAGE_PUNT",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_USER_INTERVENTION: {
+                "title": "USER_INTERVENTION",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_OUT_OF_MEMORY: {
+                "title": "OUT_OF_MEMORY",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_DOOR_OPEN: {"title": "DOOR_OPEN", "usable": True},
+            win32print.PRINTER_STATUS_SERVER_UNKNOWN: {
+                "title": "SERVER_UNKNOWN",
+                "usable": False,
+            },
+            win32print.PRINTER_STATUS_POWER_SAVE: {
+                "title": "POWER_SAVE",
+                "usable": True,
+            },
+        }
+    else:
+        from escpos.printer import Usb as POSDriver
+
+except ImportError:
+    installed = False
+    print("ESCPOS: xmlescpos python library not installed")
+else:
+
+    class ESCPOSDriver(ThreadDriver, POSDriver):
+        """ESCPOS Printer Driver class for pywebdriver"""
+
+        def __init__(self, *args, **kwargs):
+            self.eprint = None
+            self.vendor_product = None
+            self.open_args = []
+            if device_type == "usb":
+                printers = self.connected_usb_devices()
+                if printers:
+                    printer = printers[0]
+                    idVendor = printer.get("vendor")
+                    idProduct = printer.get("product")
+                    usb_args = {"idVendor": idVendor, "idProduct": idProduct}
+                    kwargs["in_ep"] = printer.get("in_ep", 0x82)
+                    kwargs["out_ep"] = printer.get("out_ep", 0x01)
+                    kwargs["timeout"] = 0
+                    POSDriver.__init__(self, idVendor, idProduct, **kwargs)
+                    self.open_args.append(usb_args)  # First open_arg is usb_args
+            elif device_type == "serial":
+                kwargs["devfile"] = config.get("escpos_driver", "serial_device_name")
+                kwargs["baudrate"] = config.getint("escpos_driver", "serial_baudrate")
+                kwargs["bytesize"] = config.getint("escpos_driver", "serial_bytesize")
+                kwargs["timeout"] = config.getint("escpos_driver", "serial_timeout")
+                POSDriver.__init__(self, **kwargs)
+            elif device_type == "win32":
+                try:
+                    kwargs["printer_name"] = config.get("escpos_driver", "printer_name")
+                except NoOptionError:
+                    config_printer_names = config.get(
+                        "escpos_driver", "printer_names"
+                    ).split(",")
+                    printers_list = win32print.EnumPrinters(
+                        win32print.PRINTER_ENUM_NAME, None, 2
+                    )
+                    printers_dict = {
+                        item["pPrinterName"]: item for item in printers_list
+                    }
+                    i = 0
+                    while i < len(config_printer_names):
+                        config_printer_name = config_printer_names[i]
+                        i += 1
+                        for printer_name, printer in printers_dict.items():
+                            if fnmatch.fnmatch(printer_name, config_printer_name):
+                                status = PRINTER_STATUS_DICT.get(printer["Status"])
+                                _logger.debug([printer_name, status])
+                                if status and "usable" in status and status["usable"]:
+                                    kwargs[
+                                        "printer_name"
+                                    ] = printer_name  # Printer will be used
+                                    break
+                        if "printer_name" in kwargs:
+                            break
+                finally:
+                    if "printer_name" not in kwargs:
+                        kwargs["printer_name"] = "escpos"  # To avoid crashing
+                    POSDriver.__init__(self, **kwargs)
+            ThreadDriver.__init__(self, *args, **kwargs)
+
+        def get_vendor_product(self):
+            return "escpos-icon"
+
+        def connected_usb_devices(self):
+            connected = []
+
+            for device in SUPPORTED_DEVICES:
+                if (
+                    usb.core.find(
+                        idVendor=device["vendor"], idProduct=device["product"]
+                    )
+                    is not None
+                ):
+                    connected.append(device)
+            return connected
+
+        def open_printer(self):
+            if self.device:
+                return
+            try:
+                self.open(*self.open_args)
+                if device_type == "win32":
+                    self.device = self.hPrinter
+            except Exception as e:
+                self.set_status("error", e)
+
+        def open_cashbox(self, printer):
+            self.open_printer()
+            self.cashdraw(2)
+            self.cashdraw(5)
+            self.close()
+
+        def get_status(self, **params):
+            messages = []
+            self.open_printer()
+            if not self.device:
+                status = "disconnected"
+            elif device_type == "serial":
+                # Maybe we could do something here to start serial
+                status = "connected"
+            elif device_type == "usb":
+                try:
+                    if self.is_online():
+                        status = "connected"
+                    else:
+                        status = "connecting"
+                    # if res['printer']['status_error']:
+                    #     status = 'error'
+                    #     messages.append(
+                    #         'Error code: %i' % res['printer']['status_error'])
+                except Exception as err:
+                    status = "error"
+                    self.device = False
+                    messages.append("Error: %s" % err)
+            elif device_type == "win32":
+                messages.append(self.printer_name)
+                result = win32print.GetPrinter(self.device, 2)
+                status = PRINTER_STATUS_DICT.get(result["Status"])
+                if status:
+                    messages.append(status["title"])
+                else:
+                    messages.append("UNKNOWN: {}".format(result["Status"]))
+                if not status or status["title"] in ["OFFLINE", "NOT_AVAILABLE"]:
+                    status = "disconnected"
+                else:
+                    status = "connected"
+            else:
+                status = "disconnected"
+            self.close()
+            return {
+                "status": status,
+                "messages": messages,
+            }
+
+        def receipt_jpeg(self, b64image):
+
+            TARGET_WIDTH = 576
+            img = Image.open(io.BytesIO(base64.b64decode(b64image)))
+            w_percent = (TARGET_WIDTH / float(img.size[0]))
+            h_size = int((float(img.size[1]) * float(w_percent)))
+
+            # LANCZOS provides clean downscaling for sharp text/lines
+            resized_img = img.resize((TARGET_WIDTH, h_size), Image.Resampling.LANCZOS)
+
+            self.open_printer()
+            self.image(resized_img)
+            self.cut()
+            self.close()
+
+        def receipt(self, content):
+            self.open_printer()
+            Layout(content).format(self)
+            self.cut()
+            self.close()
+
+        def printstatus(self, eprint):
+            addr_lines = []
+            for ifaceName in interfaces():
+                addresses = [
+                    i["addr"]
+                    for i in ifaddresses(ifaceName).setdefault(
+                        AF_INET, [{"addr": "No IP addr"}]
+                    )
+                ]
+                addr_lines.append(
+                    "<p>" + ",".join(addresses) + " (" + ifaceName + ")" + "</p>"
+                )
+            msg = (
+                _(
+                    """
+                   <div align="center">
+                        <h4>PyWebDriver Software Status</h4>
+                        <br/><br/>
+                        <h5>IP Addresses:</h5>
+                        %s<br/>
+                        Port: %i
+                   </div>
+            """
+                )
+                % (
+                    "".join(addr_lines),
+                    config.getint("flask", "port"),
+                )
+            )
+            self.receipt(msg)
+
+    driver = ESCPOSDriver(app.config)
+    drivers["escpos"] = driver
+    installed = True
+
+    @app.route("/hw_proxy/default_printer_action", methods=["POST", "GET", "PUT"])
+    def default_printer_action():
+        """For Odoo 13.0+"""
+
+        action = request.json["params"]["data"]["action"]
+        if action == "print_receipt":
+            receipt = request.json["params"]["data"]["receipt"]
+            driver.push_task("receipt_jpeg", receipt)
+
+            TARGET_WIDTH = 576
+            img = Image.open(io.BytesIO(base64.b64decode(receipt)))
+            w_percent = (TARGET_WIDTH / float(img.size[0]))
+            h_size = int((float(img.size[1]) * float(w_percent)))
+
+            # LANCZOS provides clean downscaling for sharp text/lines
+            resized_img = img.resize((TARGET_WIDTH, h_size), Image.Resampling.LANCZOS)
+
+            printer = POSLanDriver("192.168.1.109", port=9100)
+            printer.image(resized_img)
+            printer.cut()
+            printer.close()
+
+        if action == "cashbox":
+            driver.push_task("open_cashbox")
+        return jsonify(jsonrpc="2.0", result=True)
+
+    @app.route("/hw_proxy/print_xml_receipt", methods=["POST", "GET", "PUT"])
+    def print_xml_receipt_json():
+        """For Odoo 8.0+"""
+
+        receipt = request.json["params"]["receipt"].replace("ean13", "EAN13")
+        driver.push_task("receipt", receipt)
+
+        return jsonify(jsonrpc="2.0", result=True)
+
+    @app.route("/print_status.html", methods=["GET"])
+    def print_status_http():
+        driver.push_task("printstatus")
+        return render_template("print_status.html")
+
+    @app.route("/hw_proxy/open_cashbox", methods=["POST", "GET", "PUT"])
+    def open_cashbox():
+        driver.push_task("open_cashbox")
+        return jsonify(jsonrpc="2.0", result=True)
